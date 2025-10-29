6008ebfc701a65cdb5fff298160f70e4ed74322b
sarthakdjoshiFlutterDev30 <dev.sarthak0001@gmail.com>
2025-10-29 21:06:24 +0530

    modify

--- sarthak/app/sitemap.ts
+++ sarthak/app/sitemap.ts
@@ -6,27 +6,23 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: baseUrl,
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 1,
+      lastModified: '2025-10-29T11:40:49+00:00',
+      priority: 1.00,
     },
     {
       url: `${baseUrl}/privacy-policy`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
+      lastModified:'>2025-10-29T11:40:49+00:00',
+      priority: 0.80,
     },
     {
       url: `${baseUrl}/terms-of-service`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
+      lastModified:'2025-10-29T11:40:50+00:00',
+      priority: 0.80,
     },
     {
       url: `${baseUrl}/cookie-policy`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
+      lastModified: '2025-10-29T11:40:50+00:00',
+      priority: 0.80,
     },
   ];
 }
