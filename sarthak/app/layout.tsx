801c13750e369f64e7af578a2b8e1d070f0c587f
sarthakdjoshiFlutterDev30 <dev.sarthak0001@gmail.com>
2025-10-29 17:08:52 +0530

    google

--- sarthak/app/layout.tsx
+++ sarthak/app/layout.tsx
@@ -6,6 +6,9 @@ export const metadata: Metadata = {
   icons:'https://media.licdn.com/dms/image/v2/D4D0BAQFUe8m5d02Q-w/company-logo_200_200/B4DZih0WwyH4AI-/0/1755061515713?e=1761782400&v=beta&t=_0-_VaZpmgI_WctUBmcCSO3g90CEZqw9JDtydP2j9lk',
   description: 'Your trusted partner for web development, mobile apps, ERP solutions, and API integration. Transform your vision into cutting-edge digital solutions.',
   keywords: 'IT services, web development, mobile apps, ERP solutions, API integration, software development,mobile app development, web development, ERP solutions, Flutter development, MERN stack, digital transformation, Bytesflare Infotech,bytes,flare,inforech',
+  verification: {
+    google: 'Nr6_ejgGp7gXH4ioPH-4mEeOi0QpgswRPJYF_y3PBOI',
+  },
 };
 
 export default function RootLayout({
