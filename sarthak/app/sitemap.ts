9fc2aefa4bc58dc0deb26c3bf3b727924b828e12
sarthakdjoshiFlutterDev30 <dev.sarthak0001@gmail.com>
2025-10-29 20:59:19 +0530

    sitemap path

--- sarthak/app/sitemap.ts
+++ sarthak/app/sitemap.ts
@@ -1,21 +1,29 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = 'https://bytesflareinfotech.vercel.app';
+  
   return [
     {
-      url: 'https://bytesflareinfotech.vercel.app/privacy-policy',
+      url: baseUrl,
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1,
     },
     {
-      url: 'https://bytesflareinfotech.vercel.app/terms-of-service',
+      url: `${baseUrl}/privacy-policy`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://bytesflareinfotech.vercel.app/cookie-policy',
+      url: `${baseUrl}/terms-of-service`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/cookie-policy`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
