98653e4561c1585111fba75815d47ea679008357
sarthakdjoshiFlutterDev30 <dev.sarthak0001@gmail.com>
2025-11-13 16:50:29 +0530

    final

--- sarthak/app/layout.tsx
+++ sarthak/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import Logo from '@/app/Logo.png';
 
 export const metadata: Metadata = {
@@ -25,7 +26,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-smooth">
-      <body>{children}</body>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-NWCMPVT5');
+        `}
+      </Script>
+      <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `
+              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NWCMPVT5"
+                height="0" width="0" style="display:none;visibility:hidden"></iframe>
+            `,
+          }}
+        />
+        {children}
+      </body>
     </html>
   );
 }
